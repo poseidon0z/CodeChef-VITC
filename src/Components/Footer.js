0491172cbd84e544f3b4045d9c4ea59493652b06
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2024-03-01 22:06:45 +0530

    Footer modified

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,90 +1,90 @@
-import React from 'react';
-import logo from '../Asstes/cc-logo.svg';
-import '../Styles/Footer.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faSquareXTwitter } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { IoLogoInstagram } from "react-icons/io";
+import { CiLinkedin } from "react-icons/ci";
+import "../Styles/Footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="doublecol">
-        <div className="column">
-          <div className="center">
-            <img src={logo} alt="Codechef club logo"></img>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
-              dolore rerum porro repellendus
-            </p>
-            <p>copyright text</p>
+    <>
+      <footer>
+        <div className="row l-footer">
+          <div className="col-sm-3 col-md-3 my-2">
+            <div className="l_footer_col">
+              <p className="l_footer_head">Events</p>
+            </div>
+            <div className="l_footer_list">
+              <div className="l_footer_link">
+                <a href="/" className="footer_link">
+                  Code Quest
+                </a>
+              </div>
+              <div className="l_footer_link">
+                <a href="/" className="footer_link">
+                  Cook-Off 2024
+                </a>
+              </div>
+              <div className="l_footer_link">
+                <a href="/" className="footer_link">
+                  Decode DSA
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="column event">
-          <div className="center">
-            <h3>Events</h3>
-            <p>Event name 1</p>
-            <p>Event name 2</p>
-            <p>Event name 3</p>
+          <div className="col-sm-3 col-md-3 my-2">
+            <div className="l_footer_col">
+              <p className="l_footer_head">Blogs</p>
+            </div>
+            <div className="l_footer_list">
+              <div className="l_footer_link">
+                <a href="/" className="footer_link">
+                  Blog 1
+                </a>
+              </div>
+              <div className="l_footer_link">
+                <a href="/" className="footer_link">
+                  Blog 2
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="doublecol">
-        <div className="column">
-          <div className="center">
-            <h3>Blogs</h3>
-            <p>Blog name 1</p>
-            <p>Blog name 2</p>
-            <p>Blog name 3</p>
+          <div className="col-sm-3 col-md-3 my-2">
+            <div className="l_footer_col">
+              <p className="l_footer_head">Our Team</p>
+            </div>
+            <div className="l_footer_list">
+              <div className="l_footer_link">
+                <a href="/" className="footer_link">
+                  Batch 2024-25
+                </a>
+              </div>
+              <div className="l_footer_link">
+                <a href="/" className="footer_link">
+                  Batch 2023-24
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="column">
-          <div className="center">
-            <h3>Socials</h3>
-            <div className="socials">
-              <a href="https://in.linkedin.com/company/codechef-vitc">
-                <FontAwesomeIcon
-                  icon={faLinkedin}
-                  size="4x"
-                  inverse
-                  fixedWidth
-                  className="icon"
-                />
+          <div className="col-sm-3 col-md-3 my-2">
+            <div className="l_footer_col">
+              <p className="l_footer_head">Follow Us</p>
+            </div>
+            <div className="l_footer_list">
+              <a href="/" className="footer_link footer-icons">
+                <IoLogoInstagram />
               </a>
-              <a href="https://www.instagram.com/codechefvit/?hl=en">
-                <FontAwesomeIcon
-                  icon={faSquareInstagram}
-                  size="4x"
-                  inverse
-                  fixedWidth
-                  className="icon"
-                />
-              </a>
-              <a href="mailto:example@example.com">
-                <FontAwesomeIcon
-                  icon={faEnvelope}
-                  size="4x"
-                  inverse
-                  fixedWidth
-                  className="icon"
-                />
-              </a>
-              <a href="https://twitter.com/codechefvit">
-                <FontAwesomeIcon
-                  icon={faSquareXTwitter}
-                  size="4x"
-                  inverse
-                  fixedWidth
-                  className="icon"
-                />
+              <a href="/" className="footer_link footer-icons">
+                <CiLinkedin />
               </a>
             </div>
           </div>
         </div>
+      </footer>
+      <div className="copy-right">
+        <p className="copy-right-para">
+          &copy; {new Date().getFullYear()} Copyright All Rights Reserved
+        </p>
       </div>
-    </footer>
+    </>
   );
 };
 
